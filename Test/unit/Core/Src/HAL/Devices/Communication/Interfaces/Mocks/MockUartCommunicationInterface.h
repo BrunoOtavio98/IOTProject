63067e36f5203d566176b57d8d1eab70d7386164
Bruno Otavio <brunootavioramos13@gmail.com>
2024-03-17 18:11:33 -0300

    IOT-6 Created some necessary mocks

---
+++ Test/unit/Core/Src/HAL/Devices/Communication/Interfaces/Mocks/MockUartCommunicationInterface.h
@@ -0,0 +1,28 @@
+#ifndef UART_COMMUNICATION_INTEFACE_H_
+#define UART_COMMUNICATION_INTERFACE_H_
+
+#include <gmock/gmock.h>
+#include "Devices/Communication/Interfaces/UartCommunicationInterface.h"
+
+namespace HAL {
+namespace Devices {
+namespace Communication {
+namespace Interfaces {
+
+class MockUartCommunicationInterface : public UartCommunicationInterface
+{
+public:
+    MockUartCommunicationInterface() : UartCommunicationInterface(BaudRates::BAUD_115200, UartNumber::UART_1) {
+
+    };
+
+    MOCK_METHOD1(WriteData, bool(const std::string&));
+    MOCK_METHOD1(ReadDataIT, bool(std::function<void(const std::string&)>));
+};
+
+}
+}
+}
+}
+
+#endif UART_COMMUNICATION_INTERFACE_H_
