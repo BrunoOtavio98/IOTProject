09c36d481982e35cb1a3ebd5730d945d8a244f1f
Bruno Otavio <brunootavioramos13@gmail.com>
2024-03-20 21:56:10 -0300

    IOT-6 Start of unit testing of modem interface

--- Test/unit/Core/Src/HAL/Devices/IOT/Interfaces/ModemInterfaceTests.cpp
+++ Test/unit/Core/Src/HAL/Devices/IOT/Interfaces/ModemInterfaceTests.cpp
@@ -22,7 +22,7 @@ class ModemInterfaceHelper : public ModemInterface {
     }
 };
 
-class ModemInterfaceTests : testing::Test
+class ModemInterfaceTests : public testing::Test
 {
 public:
     ModemInterfaceTests() : 
@@ -38,6 +38,43 @@ std::shared_ptr<MockUartCommunicationInterface> uart_modem_;
 std::shared_ptr<MockDebugController> debug_controller_;
 ModemInterfaceHelper modem_;
 };
+
+TEST_F(ModemInterfaceTests, TestCommandFormat) {
+   ModemInterface::ATCommands current_cmd = ModemInterface::ATCommands::COPS;
+   ModemInterface::AtCommandTypes current_cmd_type = ModemInterface::AtCommandTypes::Test;
+
+   std::list<std::string> list_of_parameters;
+
+   std::string expect_msg_format = "AT+COPS=?";
+   EXPECT_CALL(*uart_modem_, WriteData(expect_msg_format));
+    
+   modem_.SendCommand(current_cmd_type, current_cmd, list_of_parameters);
+}
+
+TEST_F(ModemInterfaceTests, ReadCommandFormat) {
+   ModemInterface::ATCommands current_cmd = ModemInterface::ATCommands::ATeC;
+   ModemInterface::AtCommandTypes current_cmd_type = ModemInterface::AtCommandTypes::Read;
+
+   std::list<std::string> list_of_parameters;
+
+   std::string expect_msg_format = "AT&C?";
+   EXPECT_CALL(*uart_modem_, WriteData(expect_msg_format));
+    
+   modem_.SendCommand(current_cmd_type, current_cmd, list_of_parameters);
+}
+
+TEST_F(ModemInterfaceTests, WriteCommandFormat) {
+   ModemInterface::ATCommands current_cmd = ModemInterface::ATCommands::ATI;
+   ModemInterface::AtCommandTypes current_cmd_type = ModemInterface::AtCommandTypes::Write;
+
+   std::list<std::string> list_of_parameters = {"1", "B", "C", "2"};
+
+   std::string expect_msg_format = "ATI=1,B,C,2";
+   EXPECT_CALL(*uart_modem_, WriteData(expect_msg_format));
+    
+   modem_.SendCommand(current_cmd_type, current_cmd, list_of_parameters);
+}
+
 
 }
 }
