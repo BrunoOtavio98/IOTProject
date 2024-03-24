0c46e3c3258d7e751eeccf68837be4edd01ce447
Bruno Otavio <brunootavioramos13@gmail.com>
2024-03-24 13:41:29 -0300

    IOT-11 Created basic methods for queue handling

--- Core/Src/HAL/RTOSWrappers/QueueWrapper.h
+++ Core/Src/HAL/RTOSWrappers/QueueWrapper.h
@@ -8,10 +8,31 @@
 #ifndef SRC_HAL_RTOSWRAPPERS_QUEUEWRAPPER_H_
 #define SRC_HAL_RTOSWRAPPERS_QUEUEWRAPPER_H_
 
+#include <memory>
+
 #ifdef FREERTOS
+#include "FreeRTOS.h"
 #include "queue.h"
+typedef QueueHandle_t GenericQueueHandle;
+#else
+typedef int* GenericQueueHandle;
 #endif
 
+namespace HAL {
+namespace RtosWrappers {
+class QueueWrapper
+{
+public:
+    QueueWrapper();
+    ~QueueWrapper();
+    GenericQueueHandle CreateQueue(int size, int item_size);
+    int QueueSpacesAvailable(GenericQueueHandle queue);
+    void QueueDelete(GenericQueueHandle queue);
+    bool QueueSend(GenericQueueHandle queue, const void *item, int ms_to_wait);
+    bool QueueReceive(GenericQueueHandle queue, void *buffer, int ms_to_wait);
+};
 
+}
+}
 
 #endif /* SRC_HAL_RTOSWRAPPERS_QUEUEWRAPPER_H_ */
