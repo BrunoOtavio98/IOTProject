3944744bb3d6c2111c479304242fe2e005a83638
Bruno Otavio <brunootavioramos13@gmail.com>
2024-07-08 16:31:08 -0300

    IOT-22 Basic structure for storage devices

---
+++ Core/Src/HAL/Storage/SDInterface.h
@@ -0,0 +1,20 @@
+
+#ifndef SRC_HAL_STORAGE_SDINTERFACE_H_
+#define SRC_HAL_STORAGE_SDINTERFACE_H_
+
+#include "Storage/StorageInterface.h"
+
+namespace HAL {
+namespace Storage {
+
+class SDInterface : public HAL::Storage::StorageInterface
+{
+public:
+    SDInterface();
+    virtual ~SDInterface();
+};
+
+}
+}
+
+#endif // SRC_HAL_STORAGE_SDINTERFACE_H_
